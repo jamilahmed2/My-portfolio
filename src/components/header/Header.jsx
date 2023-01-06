14c520529779c7e640cf7acd1db25cdc7afa7a3e
jamilahmed0x1 <ja093599@gmail.com>
2023-01-07 02:51:07 +0500

    nav

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,27 +6,24 @@ import { HeaderSocials } from "./HeaderSocials";
 
 const Header = () => {
   return (
-    <>
-      <header>
-        <div className="container header__container">
-          <div className="container content">
-            <h5>Hello I'm</h5>
-            <h1>Jamil Ahmed</h1>
-            <h5 className="text-light">Web Developer</h5>
-            <CTA />
-          </div>
-          <HeaderSocials />
-
+    <header>
+      <div className="container header__container">
+        <div className="content">
+          <h5>Hello I'm</h5>
+          <h1>Jamil Ahmed</h1>
+          <h5 className="text-light">Web Developer</h5>
+          <CTA />
+        </div>
+        <HeaderSocials />
           <div className="me">
             <img src={ME} alt="me" />
           </div>
-
-          <a href="#contact" className="scroll__down">
-            Scroll Down
-          </a>
-        </div>
-      </header>
-    </>
+       
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
   );
 };
 
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import "./testimonials.css"
 const Testimonials = () => {
   return (
-    <div>Testimonials</div>
+    <section id='testimonials'>Testimonials</section>
   )
 }
 

ad5c1fc76ee96e1c6ec1c976d233e601d0e7e9bd
jamilahmed0x1 <ja093599@gmail.com>
2023-01-08 22:18:24 +0500

    services

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,10 +1,77 @@
 import React from 'react'
 import "./services.css"
+import {BiCheck} from "react-icons/bi"
 
-const Services = () => {
+const services = () => {
   return (
-    <section id='services'>Services</section>
+    <section id='services'>
+      <h5>What I Offer</h5>
+      <h2>services</h2>
+
+      <div className="container services__container">
+        <articles className="service">
+          <div className="service__head">
+            <h3>Designing</h3>
+          </div>
+
+          <ul className="service__list">
+            <li><BiCheck className='service__list-icon'/>
+              <p>Photoshop</p>
+            </li>
+            <li><BiCheck className='service__list-icon'/>
+             <p>Illustrator</p>
+            </li>
+            <li><BiCheck className='service__list-icon'/>
+             <p>Figma</p>
+            </li>
+          </ul>
+        </articles>
+        {/* ========= END of UI/UX ============ */}
+
+        <articles className="service">
+          <div className="service__head">
+            <h3>Frontend</h3>
+          </div>
+
+          <ul className="service__list">
+            <li><BiCheck className='service__list-icon'/>
+              <p>HTML</p>
+            </li>
+            <li><BiCheck className='service__list-icon'/>
+             <p>CSS</p>
+            </li>
+            <li><BiCheck className='service__list-icon'/>
+             <p>Javascript</p>
+            </li>
+            <li><BiCheck className='service__list-icon'/>
+             <p>Bootstrap</p>
+            </li>
+            <li><BiCheck className='service__list-icon'/>
+             <p>React</p>
+            </li>
+          </ul>
+        </articles>
+
+        <articles className="service">
+          <div className="service__head">
+            <h3>Backend</h3>
+          </div>
+
+          <ul className="service__list">
+            <li><BiCheck className='service__list-icon'/>
+              <p>Node.js</p>
+            </li>
+            <li><BiCheck className='service__list-icon'/>
+             <p>Express.js</p>
+            </li>
+            <li><BiCheck className='service__list-icon'/>
+             <p>MongoDB</p>
+            </li>
+          </ul>
+        </articles>
+      </div>
+    </section>
   )
 }
 
-export default Services
+export default services
